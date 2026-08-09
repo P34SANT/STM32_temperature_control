4a91b1068a3fb0722ebfea003999f09353f4bba4
Peasant <polishedpeasant@proton.me>
2026-08-09 23:36:52 +0330

    changed adc pin

--- Core/Src/adc.c
+++ Core/Src/adc.c
@@ -62,7 +62,7 @@ void MX_ADC1_Init(void)
 
   /** Configure for the selected ADC regular channel its corresponding rank in the sequencer and its sample time.
   */
-  sConfig.Channel = ADC_CHANNEL_4;
+  sConfig.Channel = ADC_CHANNEL_5;
   sConfig.Rank = 1;
   sConfig.SamplingTime = ADC_SAMPLETIME_3CYCLES;
   if (HAL_ADC_ConfigChannel(&hadc1, &sConfig) != HAL_OK)
@@ -89,7 +89,7 @@ void HAL_ADC_MspInit(ADC_HandleTypeDef* adcHandle)
 
     __HAL_RCC_GPIOA_CLK_ENABLE();
     /**ADC1 GPIO Configuration
-    PA4     ------> ADC1_IN4
+    PA5     ------> ADC1_IN5
     */
     GPIO_InitStruct.Pin = NTC_Pin;
     GPIO_InitStruct.Mode = GPIO_MODE_ANALOG;
@@ -133,7 +133,7 @@ void HAL_ADC_MspDeInit(ADC_HandleTypeDef* adcHandle)
     __HAL_RCC_ADC1_CLK_DISABLE();
 
     /**ADC1 GPIO Configuration
-    PA4     ------> ADC1_IN4
+    PA5     ------> ADC1_IN5
     */
     HAL_GPIO_DeInit(NTC_GPIO_Port, NTC_Pin);
 
