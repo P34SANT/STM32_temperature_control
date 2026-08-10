775204bcf1204a359ef87a577022f2386fbda5a8
Peasant <polishedpeasant@proton.me>
2026-08-10 15:21:29 +0330

    heater task and functionalities

--- Core/App/Include/heater.h
+++ Core/App/Include/heater.h
@@ -4,6 +4,11 @@
 
 #ifndef STM32TEMPCONTROL_HEATER_H
 #define STM32TEMPCONTROL_HEATER_H
+#include "main.h"
+
+void heater_ON();
+void heater_OFF();
+
 void StartHeaterTask(void *argument);
 
 #endif //STM32TEMPCONTROL_HEATER_H
--- Core/App/Source/heater.c
+++ Core/App/Source/heater.c
@@ -5,13 +5,61 @@
 #include "../Include/heater.h"
 #include "cmsis_os.h"
 
+#define HYSTERSIS_C 1
+
+heater_state_t old_state = heater_off;
+
+void heater_ON() {
+    HAL_GPIO_WritePin(
+        RGB_GREEN_GPIO_Port,
+        RGB_GREEN_Pin,
+        GPIO_PIN_RESET
+    );
+}
+
+void heater_OFF() {
+    HAL_GPIO_WritePin(
+        RGB_GREEN_GPIO_Port,
+        RGB_GREEN_Pin,
+        GPIO_PIN_SET
+    );
+}
+
+
 void StartHeaterTask(void *argument)
 {
-
     /* Infinite loop */
     for(;;)
     {
-        osDelay(1);
-    }
+        if (system_status.mode == AUTO) {
 
+            if ((float)system_status.current_temp <
+                (float)system_status.target_temp - HYSTERSIS_C) {
+
+                if (old_state != heater_on) {
+                    system_status.heater_state = heater_on;
+                    heater_ON();
+                    old_state = heater_on;
+                }
+
+                }else if ((float)system_status.current_temp >
+                          (float)system_status.target_temp + HYSTERSIS_C) {
+
+                    if (old_state != heater_off) {
+                        system_status.heater_state = heater_off;
+                        heater_OFF();
+                        old_state = heater_off;
+                    }
+                          }
+
+        }else if (system_status.mode == MANUAL) {
+
+            heater_OFF();
+            system_status.heater_state = heater_off;
+            old_state = heater_off;
+
+        }
+
+        osDelay(10);
+    }
 }
