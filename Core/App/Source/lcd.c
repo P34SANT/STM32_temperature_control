348f6771eb3ecd3344c06ead15f507e0cb4c2e98
Peasant <polishedpeasant@proton.me>
2026-08-10 15:25:27 +0330

    fixing lcd bugs

--- Core/App/Source/lcd.c
+++ Core/App/Source/lcd.c
@@ -31,19 +31,26 @@ void StartLcdTask(void *argument)
             mode = "Manual";
         }else {
             mode = "Auto";
+            ALCD_putsXY( 13 , 1 , "   ");
+
         }
         switch (system_status.state) {
             case Init:
                 state = "Init";
+                ALCD_putsXY( 4 , 1 , "   ");
+
                 break;
             case Normal :
                 state = "Normal";
+                ALCD_putsXY( 6 , 1 , " ");
                 break;
             case Warning:
                 state = "Warning";
                 break;
             case Error:
                 state = "Error";
+                ALCD_putsXY( 5 , 1 , "  ");
+
                 break;
         }
         ALCD_printfXY(0 , 0 , "Trgt:%02d  Crnt:%02d" , system_status.target_temp , system_status.current_temp);
